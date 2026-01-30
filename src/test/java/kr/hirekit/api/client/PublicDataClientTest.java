3cef325e56f4a9c0aece10fb7d74aede17abf875
leehydev <leehydev@gmail.com>
2026-01-31 08:14:30 +0900

    테스트

---
+++ src/test/java/kr/hirekit/api/client/PublicDataClientTest.java
@@ -0,0 +1,30 @@
+package kr.hirekit.api.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PublicDataClientTest {
+
+    @Autowired
+    private PublicDataClient publicDataClient;
+
+    @Test
+    void getCorpOutline_법인등록번호로_조회() {
+        String result = publicDataClient.getCorpOutline("1101113892240", null);
+
+        assertNotNull(result);
+        System.out.println(result);
+    }
+
+    @Test
+    void getCorpOutline_법인명으로_조회() {
+        String result = publicDataClient.getCorpOutline(null, "메리츠자산운용");
+
+        assertNotNull(result);
+        System.out.println(result);
+    }
+}
